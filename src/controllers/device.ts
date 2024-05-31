0bbcff18a6cc8fee124f3bf2ba09015335236552
Gosudarev Danila <danidagosudarev@gmail.com>
2024-05-31 22:48:26 +0300

    controllers for devices

--- src/controllers/device.ts
+++ src/controllers/device.ts
@@ -1,4 +1,8 @@
 import { Request, Response } from "express";
+import { v4 as uuidv4 } from "uuid";
+import path from "path";
+import prisma from "../prisma/prisma-client.js";
+import { off } from "process";
 
 /**
  * @route POST /api/device/create
@@ -6,16 +10,123 @@ import { Request, Response } from "express";
  * @access Private
  */
 const create = async (req: Request, res: Response) => {
-  res.send("create");
+  try {
+    const data = req.body;
+    const { img }: any = req.files;
+
+    let fileName = uuidv4() + ".jpg";
+    const __dirname = import.meta.dirname;
+
+    img.mv(path.resolve(__dirname, "..", "static", fileName));
+
+    if (!data.name || !data.price || !data.brandId || !data.typeId) {
+      return res.status(400).json({
+        message: "Все поля обязательные",
+      });
+    }
+
+    const device = await prisma.device.create({
+      data: {
+        name: data.name,
+        price: parseInt(data.price),
+        brandId: parseInt(data.brandId),
+        typeId: parseInt(data.typeId),
+        img: fileName,
+      },
+    });
+
+    interface IInfo {
+      title: string;
+      description: string;
+    }
+
+    if (data.info) {
+      const info = JSON.parse(data.info);
+      info.forEach((i: IInfo) =>
+        prisma.deviceInfo.create({
+          data: {
+            title: i.title,
+            description: i.description,
+            deviceId: device.id,
+          },
+        })
+      );
+    }
+
+    res.status(201).json(device);
+  } catch (error) {
+    res.status(500).json({
+      message: `Что-то пошло не так`,
+    });
+  }
 };
 
 /**
  * @route GET /api/device/
- * @desc Получение всех товаров
+ * @desc Получение всех товаров + фильтрация
  * @access Public
  */
 const getAll = async (req: Request, res: Response) => {
-  res.send("getAll");
+  try {
+    let { brandId, typeId, limit, page }: any = req.query;
+    page = +page || 1;
+    limit = +limit || 9;
+    let offset = page * limit - limit;
+    let devices;
+
+    // /api/device/
+    if (!brandId && !typeId) {
+      devices = await prisma.device.findMany({
+        take: limit,
+        skip: offset,
+      });
+    }
+
+    // /api/device/?brandId=1
+    if (brandId && !typeId && typeof brandId === "string") {
+      devices = await prisma.device.findMany({
+        where: {
+          brandId: parseInt(brandId),
+        },
+        skip: offset,
+        take: limit,
+      });
+    }
+
+    // /api/device/?typeId=1
+    if (!brandId && typeId && typeof typeId === "string") {
+      devices = await prisma.device.findMany({
+        where: {
+          typeId: parseInt(typeId),
+        },
+        skip: offset,
+        take: limit,
+      });
+    }
+
+    // /api/device/?brandId=1&typeId=1
+    if (
+      brandId &&
+      typeId &&
+      typeof typeId === "string" &&
+      typeof brandId === "string"
+    ) {
+      devices = await prisma.device.findMany({
+        where: {
+          brandId: parseInt(brandId),
+          typeId: parseInt(typeId),
+        },
+        skip: offset,
+        take: limit,
+      });
+    }
+
+    res.status(200).json(devices);
+  } catch (error) {
+    res.status(500).json({
+      message: "Не удалось получить товары",
+    });
+  }
 };
 
 /**
@@ -24,7 +135,24 @@ const getAll = async (req: Request, res: Response) => {
  * @access Public
  */
 const getOne = async (req: Request, res: Response) => {
-  res.send("get one");
+  try {
+    const { id } = req.params;
+
+    const device = await prisma.device.findUnique({
+      where: {
+        id: +id,
+      },
+      include: {
+        deviceInfo: true,
+      },
+    });
+
+    res.status(200).json(device);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Не удалось получить товар",
+    });
+  }
 };
 
 export { create, getAll, getOne };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import logger from 'morgan';
+import logger from "morgan";
+import fileUpload from "express-fileupload";
+import path from "path";
 import router from "./routes/index.js";
 
 dotenv.config();
@@ -9,10 +11,14 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 5000;
 
+const __dirname = import.meta.dirname;
+
 app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use(fileUpload({}));
 
 app.use("/api", router);
 
